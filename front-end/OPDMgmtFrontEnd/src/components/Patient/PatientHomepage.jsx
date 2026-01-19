30a029ae6991886ca54f05215751d9a58b237d3e
Nabhag2507 <nabhagsorathiya81@gmail.com>
2026-01-19 16:47:28 +0530

    Remove unused components and add foundational files for authentication and layout

--- front-end/OPDMgmtFrontEnd/src/components/Patient/PatientHomepage.jsx
+++
@@ -1,9 +0,0 @@
-function PatientHomepage(){
-    return(
-        <>
-            <h1>This is patients master page</h1>
-        </>
-    )
-}
-
-export default PatientHomepage;
--- front-end/OPDMgmtFrontEnd/src/components/treatment/TreatementHomepage.jsx
+++
@@ -1,9 +0,0 @@
-function TreatementHomepage(){
-    return(
-        <>
-            <h1>This is Treatement home page</h1>
-        </>
-    )
-}
-
-export default TreatementHomepage;
